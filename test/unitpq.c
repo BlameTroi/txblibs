2b4d1c4ab15bd62f96b475f16b578d56cffa76b8
troi <blametroi@gmail.com>
2024-09-30 17:31:18 -0400

    pq redo and consistency

--- test/unitpq.c
+++ test/unitpq.c
@@ -2,16 +2,14 @@
 
 /* released to the public domain, troy brumley, may 2024 */
 
-#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "minunit.h"
 
 #include "../inc/misc.h"
 #include "../inc/rand.h"
+#include "../inc/str.h"
 
 #include "../inc/pq.h"
 
@@ -21,11 +19,8 @@
 
 #define RAND_SEED 6803
 
-/* each test will have id and payload lists available to work with. */
-
 void
 test_setup(void) {
-
 	/* let's use a different seed than 1, but not time() because i want
 	   repeatable tests. */
 	set_random_generator(RAND_DEFAULT);
@@ -33,87 +28,116 @@ test_setup(void) {
 
 }
 
-
-/* after each test, release the items and lists properly. */
-
 void
 test_teardown(void) {
-
 }
-
-bool test_threaded = false;
 
 /*
  * the priority queue tests:
  */
 
-MU_TEST(test_pq_create) {
-	pqcb *pq = pq_create(test_threaded);
+MU_TEST(test_create) {
+	pqcb *pq = pq_create();
 	mu_should(pq);
 	mu_should(pq_destroy(pq));
 }
 
-MU_TEST(test_pq_empty) {
-	pqcb *pq = pq_create(test_threaded);
+MU_TEST(test_empty) {
+	pqcb *pq = pq_create();
 	mu_should(pq_empty(pq));
 	mu_should(pq_count(pq) == 0);
 	mu_should(pq_destroy(pq));
 }
 
-MU_TEST(test_pq_access_empty) {
-	pqcb *pq = pq_create(test_threaded);
-	mu_should(pq_peek(pq) == NULL);
-	mu_should(pq_get(pq) == NULL);
+MU_TEST(test_access_empty) {
+	pqcb *pq = pq_create();
+	long priority;
+	void *payload;
+	mu_shouldnt(pq_peek_highest(pq, &priority, &payload));
+	mu_shouldnt(pq_peek_lowest(pq, &priority, &payload));
+	mu_shouldnt(pq_get_highest(pq, &priority, &payload));
+	mu_shouldnt(pq_get_lowest(pq, &priority, &payload));
 	mu_should(pq_destroy(pq));
 }
 
-MU_TEST(test_pq_add_first) {
-	pqcb *pq = pq_create(test_threaded);
-	pq_put(pq, 100, "100");
+MU_TEST(test_insert) {
+	pqcb *pq = pq_create();
+	pq_insert(pq, 100, "100");
 	mu_shouldnt(pq_empty(pq));
 	mu_should(pq_count(pq) == 1);
 	mu_shouldnt(pq_destroy(pq));
-	char *str = (char *)pq_get(pq);
-	assert(str);
-	mu_should(strcmp(str, "100") == 0);
+	long priority;
+	void *payload;
+	mu_should(pq_get_highest(pq, &priority, &payload));
+	mu_should(equal_string(payload, "100"));
 	mu_should(pq_empty(pq));
 	mu_should(pq_count(pq) == 0);
 	mu_should(pq_destroy(pq));
 }
 
-MU_TEST(test_pq_read_loop) {
-	pqcb *pq = pq_create(true);
-	pq_put(pq, 100, "100");
-	pq_put(pq, 99, "99");
-	pq_put(pq, 101, "101");
-	char *str = NULL;
+MU_TEST(test_read_loop) {
+	pqcb *pq = pq_create();
+	pq_insert(pq, 100, "100");
+	pq_insert(pq, 99, "99");
+	pq_insert(pq, 101, "101");
 	int i = 0;
-	while (str = pq_get(pq), str)
+	long priority;
+	void *payload;
+	while (pq_get_highest(pq, &priority, &payload))
 		i += 1;
-	mu_should(i = 3);
+	mu_should(i == 3);
 	mu_should(pq_empty(pq));
 	mu_should(pq_destroy(pq));
 }
 
-MU_TEST(test_pq_rand_volume) {
-	pqcb *pq = pq_create(test_threaded);
-	pq_put(pq, 1024, (void *)1024);
-	pq_put(pq, 8888, (void *)8888);
-	pq_put(pq, -3, (void *)-3);
+MU_TEST(test_peek_high_low) {
+	pqcb *pq = pq_create();
+	pq_insert(pq, 100, "100");
+	pq_insert(pq, 99, "99");
+	pq_insert(pq, 101, "101");
+
+	long priority;
+	void *payload;
+
+	pq_peek_highest(pq, &priority, &payload);
+	mu_should(priority = 101);
+	mu_should(equal_string(payload, "101"));
+
+	pq_peek_lowest(pq, &priority, &payload);
+	mu_should(priority = 99);
+	mu_should(equal_string(payload, "99"));
+
+	int i = 0;
+	while (pq_get_highest(pq, &priority, &payload))
+		i += 1;
+	mu_should(i == 3);
+	mu_should(pq_empty(pq));
+	mu_should(pq_destroy(pq));
+}
+
+MU_TEST(test_random_volume) {
+	pqcb *pq = pq_create();
+	pq_insert(pq, 1024, (void *)1024);
+	pq_insert(pq, 8888, (void *)8888);
+	pq_insert(pq, -3, (void *)-3);
 	mu_shouldnt(pq_empty(pq));
 	mu_should(pq_count(pq) == 3);
 	for (int i = 0; i < 10000; i++) {
 		long j = random_between(0, 99999);
-		pq_put(pq, j, (void *)j);
+		pq_insert(pq, j, (void *)j);
 	}
 	mu_shouldnt(pq_empty(pq));
 	mu_should(pq_count(pq) == 10003);
-	void *last_pri = pq_peek(pq);
+	long last_pri;
+	long priority;
+	void *payload;
+	mu_should(pq_peek_highest(pq, &priority, &payload));
+	last_pri = priority;
 	while (!pq_empty(pq)) {
-		void *this_pri =  pq_get(pq);
-		if ((long)this_pri > (long)last_pri)
-			mu_should((long)this_pri <= (long)last_pri);
-		last_pri = this_pri;
+		pq_get_highest(pq, &priority, &payload);
+		if (priority > last_pri)
+			mu_should((priority <= last_pri));
+		last_pri = priority;
 	}
 	printf("\n");
 	mu_should(pq_empty(pq));
@@ -136,26 +160,15 @@ MU_TEST_SUITE(test_suite) {
 
 	/* run your tests here */
 
-	test_threaded = false;
-	printf("\n\npriority queue -- not threaded \n\n");
-	MU_RUN_TEST(test_pq_create);
-	MU_RUN_TEST(test_pq_empty);
-	MU_RUN_TEST(test_pq_access_empty);
-	MU_RUN_TEST(test_pq_add_first);
-	MU_RUN_TEST(test_pq_read_loop);
-	MU_RUN_TEST(test_pq_rand_volume);
-
-	test_threaded = true;
-	printf("\n\npriority queue -- threaded \n\n");
-	MU_RUN_TEST(test_pq_create);
-	MU_RUN_TEST(test_pq_empty);
-	MU_RUN_TEST(test_pq_access_empty);
-	MU_RUN_TEST(test_pq_add_first);
-	MU_RUN_TEST(test_pq_read_loop);
-	MU_RUN_TEST(test_pq_rand_volume);
-
+	printf("\n\npriority queue\n\n");
+	MU_RUN_TEST(test_create);
+	MU_RUN_TEST(test_empty);
+	MU_RUN_TEST(test_access_empty);
+	MU_RUN_TEST(test_insert);
+	MU_RUN_TEST(test_read_loop);
+	MU_RUN_TEST(test_random_volume);
+	MU_RUN_TEST(test_peek_high_low);
 }
-
 
 /*
  * master control:
@@ -167,3 +180,4 @@ main(int argc, char *argv[]) {
 	MU_REPORT();
 	return MU_EXIT_CODE;
 }
+/* unitpq.c ends here */
